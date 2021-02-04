33e7d228033c48d3597c146ca22862d7cbf5b350
Dian Leithon Isidro <dlv.isidro@gmail.com>
2021-02-04 17:13:40 +0800

    show MNL timezone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
   title = 'server-time-viewer';
   date: Date;
   toTz: String = 'Asia/Manila';
-  fromTz: String = 'UTC +02:00';
+  fromTz: String = 'Asia/Manila';
   mnlDate: Date;
 
   constructor() {
@@ -21,21 +21,12 @@ export class AppComponent {
   async fetchServerTime() {
     console.log('Fetching server time');
     let d = await axios.get(`${environment.avaloqService}/time`);
-    console.log(d);
     let serverDate = new Date(d.data['iso8601']);
     this.date = serverDate;
-    console.log(serverDate);
-    let t = await axios.get(`${environment.timezonedbService}/v2.1/convert-time-zone?key=${environment.timezonedbApiKey}&time=${serverDate.valueOf()}&format=json&from=${this.fromTz}&to=${this.toTz}`);
-    console.log(t);
+    let fromTz = d.data['timezone'];
+    let t = await axios.get(`${environment.timezonedbService}/v2.1/convert-time-zone?key=${environment.timezonedbApiKey}&time=${serverDate.valueOf()}&format=json&from=${fromTz}&to=${this.toTz}`);
 
     let toTz = new Date(t.data['toTimestamp']);
-    console.log(toTz);
     this.mnlDate = toTz;
-    console.log('server iso ', d.data['iso8601'])
-    console.log('server ', serverDate.valueOf());
-    console.log('mnl ', toTz.valueOf());
-    console.log(toTz.valueOf() - serverDate.valueOf());
-    console.log(serverDate);
-    console.log(toTz);
   }
 }
